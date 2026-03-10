2f6c4e75ace83c68c4dd38226eb967645cddff4f
anteksz24 <szantek2004@gmail.com>
2026-03-10 18:42:27 +0100

    Add information about last data update

--- dashboard/main.py
+++ dashboard/main.py
@@ -7,6 +7,8 @@ import sys
 class DataframeRenderer:
     def render_frame(self):
         data = json.loads(requests.get(url = sys.argv[1]).text)
+        datetime = data["DT"]
+        data.pop("DT")
         keys_list = list(data.keys())
         values_list = list(data.values())
 
@@ -16,8 +18,9 @@ class DataframeRenderer:
                 "Value": values_list,
             }
         )
-
+        
         st.dataframe(dataframe, hide_index=True)
+        st.write("Last data update: ", datetime)
 
 renderer = DataframeRenderer()
 renderer.render_frame()
