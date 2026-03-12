dfbab9a9a509f555e69b0eb0d4a81d6f13e0b4fb
anteksz24 <szantek2004@gmail.com>
2026-03-12 22:16:10 +0100

    Refactor API code and create /range endpoint returning data in set range

--- api/main.py
+++ api/main.py
@@ -8,20 +8,28 @@ from sqlalchemy.sql import text
 
 app = FastAPI()
 
-@app.get("/")
-def get_data(db: Session = Depends(get_database_session)):
-    try:
-        result = db.execute(text('SELECT row_to_json(meteo_data) FROM meteo_data ORDER BY "DT" DESC LIMIT 1'))
-        row = result.fetchone()
-        return row[0] if row else Response(status_code = status.HTTP_404_NOT_FOUND)
-    except:
-        return Response(status_code = status.HTTP_500_INTERNAL_SERVER_ERROR)
+def extract_query_result(query_result):
+    data_list = [row[0] for row in query_result]
+    return data_list[0] if len(data_list) == 1 else data_list
+
+@app.get("/latest/")
+def get_latest_data(limit: int = 1, db: Session = Depends(get_database_session)):
+    query = text('SELECT row_to_json(meteo_data) FROM meteo_data ORDER BY "DT" DESC LIMIT :limit')
+    query_result = db.execute(query, {"limit": limit})
+    measurements = extract_query_result(query_result)
+    return measurements if measurements else Response(status_code = status.HTTP_404_NOT_FOUND)
+
+@app.get("/range/")
+def get_data_in_range(start: str, end: str, db: Session = Depends(get_database_session)):
+    query = text('SELECT row_to_json(meteo_data) FROM meteo_data WHERE "DT" BETWEEN :start AND :end ORDER BY "DT" DESC')
+    query_result = db.execute(query, {"start": start, "end": end})
+    measurements = extract_query_result(query_result)
+    return measurements if measurements else Response(status_code = status.HTTP_404_NOT_FOUND)
 
 @app.post("/post/")
 def post_data(received_data: DataRequestSchema, db: Session = Depends(get_database_session)):
     if received_data.password == os.getenv("METEO_PASSWORD"):
-        meteo_dict = received_data.content.model_dump()
-        meteo_entry = MeteoDataModel(**meteo_dict)
+        meteo_entry = MeteoDataModel(**received_data.content.model_dump())
         db.add(meteo_entry)
         db.commit()
         db.refresh(meteo_entry)
