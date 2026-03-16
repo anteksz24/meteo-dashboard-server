bb3d548a5b332299b893c52710c8a8c78c665e51
anteksz24 <szantek2004@gmail.com>
2026-03-16 21:25:40 +0100

    Add Streamlit page config

--- dashboard/main.py
+++ dashboard/main.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title = "Meteo Dashboard",
+    page_icon = "⛅"
+)
+
 with st.spinner("Loading page..."):
     pg = st.navigation([st.Page("measurements.py")], position = "top")
     pg.run()
