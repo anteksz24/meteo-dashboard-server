987b05d4900cba177d93b8a19a9c5e4c63c69f79
anteksz24 <szantek2004@gmail.com>
2026-03-19 20:52:13 +0100

    Add select and date input widgets to visualizations page

--- dashboard/visualizations.py
+++ dashboard/visualizations.py
@@ -1,18 +1,27 @@
 import requests, sys, json, streamlit as st, pandas as pd
 from formatters import Formatters
+from values import Values
+from datetime import datetime, timedelta
 
 f = Formatters()
 
-code = st.text_input("Vaisala code")
-start_date = st.text_input("Start date")
-end_date = st.text_input("End date")
+code = st.selectbox(
+    label = "Parameter",
+    options = Values.code_descriptions.keys(),
+    format_func = lambda x: Values.code_descriptions[x],
+    index = None
+)
+start_date = st.date_input(label = "Start date", value = datetime.today() - timedelta(days = 1))
+end_date = st.date_input(label = "End date")
 
 if st.button("Generate chart"):
-    range_data = json.loads(requests.get(sys.argv[1] + f"/range?start={start_date}&end={end_date}").text)
+    if code == None:
+        st.error("Select parameter from the list.")
+    else:
+        range_data = json.loads(requests.get(sys.argv[1] + f"/range?start={start_date}&end={end_date}").text)
+        timestamps = f.get_values(range_data, "DT", False)
+        measurements = f.get_values(range_data, code, False)
 
-    timestamps = f.get_values(range_data, "DT", False)
-    measurements = f.get_values(range_data, code, False)
+        dataframe = pd.DataFrame({"DT": timestamps, code: measurements}).set_index("DT")
 
-    dataframe = pd.DataFrame({"DT": timestamps, code: measurements}).set_index("DT")
-
-    st.line_chart(dataframe)
+        st.line_chart(dataframe)
