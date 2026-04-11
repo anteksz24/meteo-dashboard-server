259b9e84891c419a5b68babec4eeeaf1770091bd
anteksz24 <szantek2004@gmail.com>
2026-04-11 15:52:00 +0200

    Move Streamlit pages to separate directory

--- dashboard/main.py
+++ dashboard/main.py
@@ -10,9 +10,9 @@ st.set_page_config(
 
 with st.spinner("Loading page..."):
     pg = st.navigation(
-        [st.Page("overview.py", title = "Overview", icon = ":material/bar_chart:", default = True), 
-         st.Page("measurements.py", title = "Measurements", icon = ":material/thermometer:"),
-         st.Page("visualizations.py", title = "Visualizations", icon = ":material/area_chart:")], position = "top")
+        [st.Page("pages/overview.py", title = "Overview", icon = ":material/bar_chart:", default = True), 
+         st.Page("pages/measurements.py", title = "Measurements", icon = ":material/thermometer:"),
+         st.Page("pages/visualizations.py", title = "Visualizations", icon = ":material/area_chart:")], position = "top")
     if warnings.check_available_warnings():
         st.warning(warnings)
     pg.run()
