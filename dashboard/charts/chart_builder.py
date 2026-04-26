c59100f91aef146cc9c33516c0c271a14aa59e1a
anteksz24 <szantek2004@gmail.com>
2026-04-26 18:17:03 +0200

    Refactor ChartBuilder class to attribute-based syntax

--- dashboard/charts/chart_builder.py
+++ dashboard/charts/chart_builder.py
@@ -7,27 +7,23 @@ class ChartBuilder:
         self.fetcher = fetcher
         self.formatter = formatter
         self.dataframe_builder = DataframeBuilder(formatter)
-    
+
+    def get_y_axis_title(self, parameters):
+        if len(parameters) == 1:
+            return f"{self.formatter.get_parameters_descriptions(parameters[0])} ({MeteoConstants.CONSTS_INFO[parameters[0]]["unit"]})"
+        else:
+            return ""
+
     def build_chart(self, config):
-        chart = (
-            alt.Chart(self.dataframe_builder.build_dataframe(self.fetcher.measurements, config))
-            .mark_line()
-            .encode(
-                x = alt.X("datetime:T", 
-                          title = ""),
-                y = alt.Y("value:Q", 
-                          title = f"{self.formatter.get_parameters_descriptions(config.parameters[0])} ({MeteoConstants.CONSTS_INFO[config.parameters[0]]["unit"]})" if len(config.parameters) == 1 else "")
-                .scale(zero = config.y_axis_zero),
-                color = alt.Color("variable:N", 
-                                  title = "Parameters", 
-                                  legend = alt.Legend(orient = "bottom", 
-                                                      labelLimit = 0, 
-                                                      columns = 1)
-                )
-            )
-            .properties(
-                height = 400 + 20 * len(config.parameters)
-            )
-        )
-        
+        chart = alt.Chart(data = self.dataframe_builder.build_dataframe(self.fetcher.measurements, config),
+                           mark = "line",
+                           height = 400 + 20 * len(config.parameters),
+                           encoding = alt.FacetedEncoding(x = alt.X(shorthand = "datetime:T",
+                                                                    title = ""),
+                                                          y = alt.Y(shorthand = "value:Q",
+                                                                    title = self.get_y_axis_title(config.parameters),
+                                                                    scale = alt.Scale(zero = config.y_axis_zero)),
+                                                          color = alt.Color(shorthand = "variable:N",
+                                                          title = "Parameters",
+                                                          legend = alt.Legend(orient = "bottom", labelLimit = 0, columns = 1))))
         return chart
