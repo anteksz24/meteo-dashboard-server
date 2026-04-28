8b2721cc6a8a15baf81b1f50132ccc74cfb4ebd1
anteksz24 <szantek2004@gmail.com>
2026-04-28 13:18:34 +0200

    Start WebSocket connections and show latest measurements in Overview and Measurements pages

--- dashboard/pages/measurements.py
+++ dashboard/pages/measurements.py
@@ -1,22 +1,26 @@
-import pandas as pd, streamlit as st
+import asyncio, pandas as pd, streamlit as st
 from utils.formatter import Formatter, Timestamp
-from utils.fetcher import StaticMeasurementsFetcher
+from utils.fetcher import WebSocketConnector
 
 formatter = Formatter()
 
-def render_frame():
-    measurements = StaticMeasurementsFetcher("latest").measurements
-    timestamp = Timestamp(measurements[0]["datetime"]).timestamp
-    data = formatter.remove_values_from_data_list(measurements, ["id", "datetime"])
+async def render_frame():
+    dataframe_placeholder = st.empty()
+    timestamp_placeholder = st.empty()
 
-    dataframe = pd.DataFrame(
-        {
-            "Parameter": formatter.get_parameters_descriptions(list(data[0].keys())),
-            "Value": formatter.get_values(data, list(data[0].keys())).values_units[0],
-        }
-    )
+    async for latest_measurement in WebSocketConnector().start_websocket_connection():
+        with dataframe_placeholder:
+            latest_measurement_timestamp = Timestamp(latest_measurement[0]["datetime"]).timestamp
+            latest_measurement_params_removed = formatter.remove_values_from_data_list(data = latest_measurement, parameters = ["id", "datetime"])
+
+            dataframe = pd.DataFrame(
+                {
+                    "Parameter": formatter.get_parameters_descriptions(parameters = list(latest_measurement_params_removed[0].keys())),
+                    "Value": formatter.get_values(data = latest_measurement_params_removed, parameters = list(latest_measurement_params_removed[0].keys())).values_units[0]
+                }
+            )
     
-    st.dataframe(dataframe, hide_index = True)
-    st.write("**Last data update:** ", timestamp)   
+            dataframe_placeholder.dataframe(dataframe, hide_index = True)
+            timestamp_placeholder.write(f"**Last data update:** {latest_measurement_timestamp}")
 
-render_frame()
+asyncio.run(render_frame())
--- dashboard/pages/overview.py
+++ dashboard/pages/overview.py
@@ -1,12 +1,12 @@
-import streamlit as st
+import asyncio, streamlit as st
 from utils.formatter import Formatter
-from utils.fetcher import StaticMeasurementsFetcher
+from utils.fetcher import StaticMeasurementsFetcher, WebSocketConnector
 
 formatter = Formatter()
 
-def render_metrics():
-    latest_data = StaticMeasurementsFetcher(endpoint_name = "latest").measurements
+async def render_metrics():
     average_data = StaticMeasurementsFetcher(endpoint_name = "average").measurements
+    metrics_placeholder = st.empty()
 
     metrics = [
         ("Air temperature", "air_temp_avg_1m"),
@@ -17,15 +17,17 @@ def render_metrics():
         ("Wind speed", "wind_speed_inst")
     ]
 
-    columns = st.columns(2)
-    for i in range(0, len(metrics)):
-        column = columns[i % 2]
-        column.metric(
-            label = metrics[i][0],
-            value = formatter.get_values(latest_data, [metrics[i][1]]).values_units[0][0],
-            chart_data = [value[0] for value in formatter.get_values(average_data, [metrics[i][1]]).values 
-                          if value[0] is not None],
-            border = True
-        )
+    async for latest_measurement in WebSocketConnector().start_websocket_connection():
+        with metrics_placeholder:
+            columns = st.columns(2)
+            for i in range(0, len(metrics)):
+                column = columns[i % 2]
+                column.metric(
+                    label = metrics[i][0],
+                    value = formatter.get_values(data = latest_measurement, parameters = [metrics[i][1]]).values_units[0][0],
+                    chart_data = [value[0] for value in formatter.get_values(data = average_data, parameters = [metrics[i][1]]).values
+                                if value[0] is not None],
+                    border = True
+                )
 
-render_metrics()
+asyncio.run(render_metrics())
