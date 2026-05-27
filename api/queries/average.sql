91e5acb6495bcda36869ba8d1e478126b6eedb65
anteksz24 <szantek2004@gmail.com>
2026-05-27 17:24:56 +0200

    Use vector average for wind direction and disable precipitation sum averaging

--- api/queries/average.sql
+++ api/queries/average.sql
@@ -6,7 +6,7 @@ FROM (
         ROUND(AVG("air_temp_avg_1m")::NUMERIC, 1) AS "air_temp_avg_1m",
         ROUND(AVG("humidity_avg_1m")::NUMERIC, 0) AS "humidity_avg_1m",
         ROUND(AVG("dewpoint_avg_1m")::NUMERIC, 1) AS "dewpoint_avg_1m",
-        ROUND(AVG("precipitation_sum_1h")::NUMERIC, 0) AS "precipitation_sum_1h",
+        --ROUND(AVG("precipitation_sum_1h")::NUMERIC, 0) AS "precipitation_sum_1h",
         ROUND(AVG("solar_rad_avg_1m")::NUMERIC, 0) AS "solar_rad_avg_1m",
         ROUND(AVG("sunshine_dur_1m")::NUMERIC, 0) AS "sunshine_dur_1m",
         ROUND(AVG("sunshine_dur_sum_1d")::NUMERIC, 0) AS "sunshine_dur_sum_1d",
@@ -21,11 +21,17 @@ FROM (
         ROUND(AVG("ground_temp_20cm_avg_1m")::NUMERIC, 1) AS "ground_temp_20cm_avg_1m",
         ROUND(AVG("ground_temp_50cm_avg_1m")::NUMERIC, 1) AS "ground_temp_50cm_avg_1m",
         ROUND(AVG("ground_temp_100cm_avg_1m")::NUMERIC, 1) AS "ground_temp_100cm_avg_1m",
-        ROUND(AVG("wind_direction_inst")::NUMERIC, 0) AS "wind_direction_inst",
+        CASE
+            WHEN sqrt(power(avg(cos(radians("wind_direction_inst"))), 2) + power(avg(sin(radians("wind_direction_inst"))), 2)) < 1e-6
+            THEN NULL
+            ELSE ROUND(mod((degrees(atan2(avg(sin(radians("wind_direction_inst"))), avg(cos(radians("wind_direction_inst"))))) + 360)::NUMERIC, 360)::NUMERIC, 0)
+        END AS "wind_direction_inst",
         ROUND(AVG("wind_speed_inst")::NUMERIC, 1) AS "wind_speed_inst",
-        ROUND(AVG("wind_direction_avg_2m")::NUMERIC, 0) AS "wind_direction_avg_2m",
-        ROUND(AVG("wind_direction_max_2m")::NUMERIC, 0) AS "wind_direction_max_2m",
-        ROUND(AVG("wind_direction_min_2m")::NUMERIC, 0) AS "wind_direction_min_2m",
+        CASE
+            WHEN sqrt(power(avg(cos(radians("wind_direction_avg_2m"))), 2) + power(avg(sin(radians("wind_direction_avg_2m"))), 2)) < 1e-6
+            THEN NULL
+            ELSE ROUND(mod((degrees(atan2(avg(sin(radians("wind_direction_avg_2m"))), avg(cos(radians("wind_direction_avg_2m"))))) + 360)::NUMERIC, 360)::NUMERIC, 0)
+        END AS "wind_direction_avg_2m",
         ROUND(AVG("wind_speed_avg_2m")::NUMERIC, 1) AS "wind_speed_avg_2m",
         ROUND(AVG("wind_speed_max_2m")::NUMERIC, 1) AS "wind_speed_max_2m",
         ROUND(AVG("wind_speed_min_2m")::NUMERIC, 1) AS "wind_speed_min_2m",
